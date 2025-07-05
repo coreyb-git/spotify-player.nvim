1778dee828e35c377bb6e21249cbccd8bc7cfedf
Corey B <coreyb@noreply.com>
2025-07-05 16:00:39 +1000

    trying to fix everything broken by bad commits

--- lua/spotify-player/init.lua
+++ lua/spotify-player/init.lua
@@ -7,36 +7,14 @@ function M.setup(opts)
 	end
 end
 
-local function Command_Update(Returned)
-	local result = Returned.stdout
-	--[[
-	print("code: " .. Returned.code)
-	print("signal: " .. Returned.signal)
-	print("stdout: " .. Returned.stdout)
-	print("stderr: " .. Returned.stderr)
-	]]
-	--
-	if Returned.code ~= 0 then
-		require("notify")
-		local notifyopts = { title = "spotify-player", timeout = 5000 }
-		local s = ""
-		local level = vim.log.levels.INFO
-		if string.find(Returned.stderr, "no playback found") then
-			s = "No Spotify player is currently running.\n\nStart Spotify first."
-		else
-			s = Returned.stderr
-			level = vim.log.levels.ERROR
-		end
-		vim.notify(s, level, notifyopts)
-	end
-end
-
 function M.PlayPause()
-	vim.system(config.command_playpause, {}, Command_Update)
+	local handle = io.popen(config.command_playpause)
+	require("spotify-player.llinit").ForcePoll()
 end
 
 function M.Next()
-	vim.system(config.command_next, {}, Command_Update)
+	local handle = io.popen(config.command_next)
+	require("spotify-player.llinit").ForcePoll()
 end
 
 vim.api.nvim_create_user_command(
--- lua/spotify-player/llinit.lua
+++ lua/spotify-player/llinit.lua
@@ -1,7 +1,9 @@
 local M = {}
 local Config = require("spotify-player.config")
 
-M.State = {
+local State = {
+	NextPoll_ms = 0,
+
 	isNull = true,
 	Playing = false,
 	AlbumTitle = "",
@@ -11,73 +13,87 @@ M.State = {
 }
 
 function M.isPlaying()
-	return M.State.Playing
+	return State.Playing
 end
 
 function M.get_icon()
-	if M.State.isNull then
+	if State.isNull then
 		return ""
 	end
-	if M.State.Playing then
+	if State.Playing then
 		return " "
 	end
 	return " "
 end
 
 function M.get_text()
-	if M.State.isNull then
+	if State.isNull then
 		return ""
 	end
-	return M.State.AlbumTitle .. ":" .. M.State.TrackTitle
+	return require("spotify-player.marquee").getText()
+	--return State.AlbumTitle .. ":" .. State.TrackTitle
 end
 
 local function Update_Callback(Returned)
 	local result = Returned.stdout
 
-	M.State.Playing = false
-	M.State.AlbumTitle = ""
+	State.Playing = false
+	State.AlbumTitle = ""
+	State.TrackTitle = ""
+
+	if Returned.code > 0 then --if error
+		return
+	end
+
 	if string.find(result, "null") == 1 then
-		M.State.isNull = true
-		M.State.AlbumTitle = "Not Playing"
+		State.isNull = true
+		State.AlbumTitle = "Not Playing"
 	else
-		M.State.isNull = false
+		State.isNull = false
 		local isPlaying = string.find(result, '"is_playing":true')
 		if isPlaying ~= nil then
-			M.State.Playing = true
+			State.Playing = true
 		end
 
-		if M.State.isNull == false then
+		if State.isNull == false then
 			local index = string.find(result, '"item":')
 			index = string.find(result, '"artists":', index)
-			index = string.find(result, '"name":"', index)
-			index = index + 8 --move to right of double quote
-			local indexend = string.find(result, '"', index) - 1
-			local AlbumTitle = string.sub(result, index, indexend)
-			M.State.AlbumTitle = AlbumTitle
+			if index == nil then --probably Spotify DJ talking, so no actual track being played.
+				State.AlbumTitle = "Spotify DJ"
+				State.TimeElapsed = 0
+				State.TimeTotal = 10000 --try to update in 10 seconds
+			else
+				index = string.find(result, '"name":"', index)
+				index = index + 8 --move to right of double quote
+				local indexend = string.find(result, '"', index) - 1
+				local AlbumTitle = string.sub(result, index, indexend)
+				State.AlbumTitle = AlbumTitle
 
-			index = string.find(result, '"is_local":')
-			index = string.find(result, '"name":"', index)
-			index = index + 8 --move to right of double quote
-			local indexend = string.find(result, '"', index) - 1
-			local TrackTitle = string.sub(result, index, indexend)
-			M.State.TrackTitle = TrackTitle
+				index = string.find(result, '"is_local":')
+				index = string.find(result, '"name":"', index)
+				index = index + 8 --move to right of double quote
+				local indexend = string.find(result, '"', index) - 1
+				local TrackTitle = string.sub(result, index, indexend)
+				State.TrackTitle = TrackTitle
 
-			index = string.find(result, '"progress_ms":')
-			index = index + 14
-			indexend = string.find(result, ",", index) - 1
-			M.State.TimeElapsed = string.sub(result, index, indexend)
+				index = string.find(result, '"progress_ms":')
+				index = index + 14
+				indexend = string.find(result, ",", index) - 1
+				State.TimeElapsed = string.sub(result, index, indexend)
 
-			index = string.find(result, '"duration_ms":')
-			index = index + 14
-			indexend = string.find(result, ",", index) - 1
-			M.State.TimeTotal = string.sub(result, index, indexend)
+				index = string.find(result, '"duration_ms":')
+				index = index + 14
+				indexend = string.find(result, ",", index) - 1
+				State.TimeTotal = string.sub(result, index, indexend)
+			end
 		end
 	end
+	require("spotify-player.marquee").setText(State.AlbumTitle, State.TrackTitle)
 
 	local ms = Config.lualine_update_max_ms
 
-	if M.State.Playing then
-		local TimeLeft = tonumber(M.State.TimeTotal) - tonumber(M.State.TimeElapsed)
+	if State.Playing then
+		local TimeLeft = tonumber(State.TimeTotal) - tonumber(State.TimeElapsed)
 		ms = TimeLeft + 1000 --add extra time so it updates after new track starts
 	end
 
@@ -88,13 +104,33 @@ local function Update_Callback(Returned)
 	if ms < Config.lualine_update_min_ms then
 		ms = Config.lualine_update_min_ms
 	end
-	vim.defer_fn(M.StateUpdate, ms)
+
+	if State.NextPoll_ms > ms then
+		State.NextPoll_ms = ms
+	end
 end
 
-function M.StateUpdate()
-	vim.system(Config.command_update, {}, Update_Callback)
+local function onError(err, data)
+	State.isNull = true
+	State.Playing = false
 end
 
-M.StateUpdate()
+function M.ForcePoll()
+	State.NextPoll_ms = 5000
+end
+
+function TimerUpdate()
+	require("spotify-player.marquee").Update()
+
+	State.NextPoll_ms = State.NextPoll_ms - Config.lualine_timer_update_ms
+	if State.NextPoll_ms <= 0 then
+		State.NextPoll_ms = Config.lualine_update_max_ms
+		vim.system({ "spotify_player", "get", "key", "playback" }, { stderr = onError }, Update_Callback)
+	end
+
+	vim.defer_fn(TimerUpdate, Config.lualine_timer_update_ms)
+end
+
+TimerUpdate()
 
 return M
